abbebebf956e2fe97b72ffc0a43ebde6bf84466a
mcsuth <seimith@gmail.com>
2014-11-06 16:41:01 -0800

    Edit new version

--- _site/script.js
+++ _site/script.js
@@ -1,20 +1,20 @@
-    $(document).ready(function() {
-      var item = $('#logo');
-      var body = $(document.body);
-      var bodyHeight = body.height();
+$(document).ready(function() {
+  var item = $('#logo');
+  var body = $(document.body);
+  var bodyHeight = body.height();
 
-      $(window).scroll(function () {
-          item.css({
-              'transform': 'rotate(' + (body.scrollTop() / bodyHeight * 3) + 'deg)'
-          });
+  $(window).scroll(function () {
+      item.css({
+          'transform': 'rotate(' + (body.scrollTop() / bodyHeight * 3) + 'deg)'
       });
-      $('.projects').hide();
-      $('.projectslink').on('click', function() {
-        $('.info').hide();
-        $('.projects').fadeIn(300);
-      })
-      $('.home').on('click', function() {
-        $('.info').fadeIn(300);
-        $('.projects').hide();
-      })
-    })
+  });
+  $('.projects').hide();
+  $('.projectslink').on('click', function() {
+    $('.info').hide();
+    $('.projects').fadeIn(300);
+  })
+  $('.home').on('click', function() {
+    $('.info').fadeIn(300);
+    $('.projects').hide();
+  })
+});
